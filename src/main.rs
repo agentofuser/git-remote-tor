304538e0f8e22a9e09f51ba05e213e9007c83203
Agent of User <git@agentofuser.com>
2019-09-27 10:12:27 -0400

    proxy http urls to git-remote-http with torsocks

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
     match env::var("GIT_DIR") {
-        Ok(val) => println!("To be implemented. GIT_DIR found: {:?}", val),
+        Ok(val) => {
+            Command::new("torsocks")
+                .arg("git-remote-http")
+                .args(env::args().skip(1))
+                .spawn()
+                .expect("Error proxying to git-remote-*");
+        }
         Err(e) => {
             eprintln!("GIT_DIR is not set: {}", e);
             std::process::exit(1)
